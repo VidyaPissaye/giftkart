82e9887d05e5a554913577393ff1a8425a31f388
Vidya Pissaye <vidya.pissaye@gmail.com>
2012-09-20 16:29:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftkart::Application.config.secret_token = '39ab27c9048e86888bf8037e20ea3061a8a00935d25da8a6b3e16f5c52ac5c19217503a7c58f0e0140c47785afe823fa571d0802c9672a0724787b50f74f8c52'
